ed0d1d2a13c433b628d30b0ee8378900d1927227
hy1107 <hitomiyabe1107@gmail.com>
2016-06-22 20:21:00 +0900

    unity test2

---
+++ test/Assets/Cube.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+	int flag = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+
+			
+	void Update () {
+		if (flag == 1) {
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				transform.Rotate (new Vector3 (0, 0, 1), +5);
+			}
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				transform.Rotate (new Vector3 (0, 0, 1), -5);
+			}
+		}
+	}
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.name == "Sphere"){
+			flag = 1;
+	    }
+	}
+}
---
+++ test/Assets/Goal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.name == "Sphere") {
+			SceneManager.LoadScene ("Goal");
+		}
+	}
+}
