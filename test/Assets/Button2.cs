365d16f67daaf8c7ce21c4dc2e71d1c303dd0628
hy1107 <hitomiyabe1107@gmail.com>
2016-06-26 12:49:09 +0900

    main2画面つなぎ完成

---
+++ test/Assets/Button2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Button2 : MonoBehaviour {
+
+	public void SceneLoad () {
+		Application.LoadLevel ("Main2");
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
---
+++ test/Assets/ButtonTop.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonTop : MonoBehaviour {
+
+	public void SceneLoad () {
+		Application.LoadLevel ("Start");
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
