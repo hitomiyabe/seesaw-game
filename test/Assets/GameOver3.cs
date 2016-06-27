5c9d7e29866f0c5cf7d084c88cd38420334f03fc
hy1107 <hitomiyabe1107@gmail.com>
2016-06-27 22:46:08 +0900

    fish

---
+++ test/Assets/GameOver3.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameOver3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.name == "Player") {
+			SceneManager.LoadScene ("GameOver3");
+		}
+	}
+}
