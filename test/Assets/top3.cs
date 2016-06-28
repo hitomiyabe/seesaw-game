b5dad562df20a5b130712e556d4752f6b238316e
hy1107 <hitomiyabe1107@gmail.com>
2016-06-28 10:00:20 +0900

    仮完成

---
+++ test/Assets/top3.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class top3 : MonoBehaviour {
+
+	public void SceneLoad () {
+		Application.LoadLevel ("Main3");
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
