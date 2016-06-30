0c0721bfd8a5c4bea09497e911582cf254cccfbf
hy1107 <hitomiyabe1107@gmail.com>
2016-06-30 10:26:33 +0900

    main4完成

---
+++ test/Assets/Goal4.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Goal4 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.name == "Player") {
+			SceneManager.LoadScene ("Goal4");
+		}
+	}
+}
---
+++ test/Assets/top4.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class top4 : MonoBehaviour {
+
+	public void SceneLoad () {
+		Application.LoadLevel ("Main4");
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
